7d7ecb7dd212923fcc8cfc9c0ce6b624be755cbc
R1nGoo <66073518+R1nGoo@users.noreply.github.com>
2020-05-28 20:53:55 +0700

    Add files via upload

---
+++ 13131/13131/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _13131
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 0;
+            int max = 200;
+            bool proverka;
+            if (min < max)
+            {
+                for (int i = min; i <= max; i++)
+                {
+                    proverka = true;
+
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            proverka = false;
+                            break;
+                        }
+                    }
+                    if (proverka)
+                        Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
